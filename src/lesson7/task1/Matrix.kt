f81f7b389ae9373c703599afbab256fc91e4c204
KamikadzeBull <kenny1634tlt@yandex.ru>
2017-12-16 21:23:49 +0300

    :/

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -1,54 +1,37 @@
 @file:Suppress("UNUSED_PARAMETER", "unused")
 package lesson7.task1
 
-/**
- * Ячейка матрицы: row = ряд, column = колонка
- */
+/* Ячейка матрицы: row = ряд, column = колонка */
 data class Cell(val row: Int, val column: Int)
 
-/**
- * Интерфейс, описывающий возможности матрицы. E = тип элемента матрицы
- */
+/* Интерфейс, описывающий возможности матрицы. E = тип элемента матрицы */
 interface Matrix<E> {
-    /** Высота */
-    val height: Int
+    /** Высота */ val height: Int
+    /** Ширина */ val width: Int
 
-    /** Ширина */
-    val width: Int
-
-    /**
-     * Доступ к ячейке.
-     * Методы могут бросить исключение, если ячейка не существует или пуста
-     */
+    /* Доступ к ячейке.
+     * Методы могут бросить исключение, если ячейка не существует или пуста */
     operator fun get(row: Int, column: Int): E
     operator fun get(cell: Cell): E
 
-    /**
-     * Запись в ячейку.
-     * Методы могут бросить исключение, если ячейка не существует
-     */
+    /* Запись в ячейку.
+     * Методы могут бросить исключение, если ячейка не существует */
     operator fun set(row: Int, column: Int, value: E)
     operator fun set(cell: Cell, value: E)
 }
 
-/**
- * Простая
+/* Простая
  *
  * Метод для создания матрицы, должен вернуть РЕАЛИЗАЦИЮ Matrix<E>.
  * height = высота, width = ширина, e = чем заполнить элементы.
- * Бросить исключение IllegalArgumentException, если height или width <= 0.
- */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+ * Бросить исключение IllegalArgumentException, если height или width <= 0. */
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> =
+        MatrixImpl(height, width, e)
 
-/**
- * Средняя сложность
+/* Средняя сложность
  *
- * Реализация интерфейса "матрица"
- */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
-
-    override val width: Int = TODO()
+ * Реализация интерфейса "матрица" */
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
 
     override fun get(row: Int, column: Int): E  = TODO()
 
